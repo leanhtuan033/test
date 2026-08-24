10c191a655dc6a74b6b99ad176f2f2a903324e21
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-08-24 20:42:48 +0700

    update

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -7,8 +7,8 @@ public class hello {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name=scanner.nextLine();
-        System.out.println("hello "+name); 
-        for (int i = 1; i <= 10; i++) {
+        System.out.println("hello "+name);
+        for (int i = 1; i <= 20; i++) {
             System.out.println(i);
         }
     }
