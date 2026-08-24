c7015f59d6cac268860dfeef1b9db330a3358616
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-08-24 19:25:14 +0700

    push code

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class hello {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name:");
-        String name=scanner.nextLine();
-        System.out.println("Hello "+name);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
