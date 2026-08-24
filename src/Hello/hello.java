a4a13e844ffa08c25223f524cb6446a0723aacae
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-08-24 20:09:50 +0700

    Sang

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -8,7 +8,7 @@ public class hello {
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
