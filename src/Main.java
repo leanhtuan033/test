0d84cb4420d0bd28cca61e7964c548e5220ef3f3
leanhtuan033 <anhtuanlw033@gmail.com>
2026-08-24 20:55:04 +0700

    upbai

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+        for  (int j = 1; j <= 5; j++) {
+            System.out.println("j = " + j);
+        }
     }
 }
